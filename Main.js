9ee994efe50bfe155caee8376fcd6ed1c9604da5
Manish Rane <114382294+Manish-Rane1@users.noreply.github.com>
2022-10-08 20:49:12 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,8 @@
 function three(){
     const arr=[23,45,78,90,100]
     document.write("Array is: "+arr+"<br>"+"<br>")
-    document.write("Using for loop: <br>")
+    document.write("output Using for loop is in console")
     for (var i in arr)
 {
-    document.write(arr[i]+"<br>")
+    console.log(arr[i]+"<br>")
 }}
