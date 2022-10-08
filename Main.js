d73254f3c7c2d8d9ecc607798779fe8fcb5e05a1
Manish Rane <manishrane182@gmail.com>
2022-10-08 18:13:28 +0530

    print 2d array

---
+++ Main.js
@@ -0,0 +1,8 @@
+function three(){
+    const arr=[23,45,78,90,100]
+    document.write("Array is: "+arr+"<br>"+"<br>")
+    document.write("Using for loop: <br>")
+    for (var i in arr)
+{
+    document.write(arr[i]+"<br>")
+}}
